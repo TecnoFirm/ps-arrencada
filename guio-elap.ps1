f636802ee297efa4455f24b98301112793f5beb2
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-07-29 20:53:44 +0200

    Arregla McAfee manual

--- guio-elap.ps1
+++ guio-elap.ps1
@@ -15,6 +15,45 @@ else
 $VerbosePreference = "SilentlyContinue"
 }
 
+# Abans de suprimir Apps, canvia la configuració de notificacions.
+# Elimina completament les notificacions:
+Write-Host "Disabling Action Center..."
+# Crea un arxiu si no existeix...
+If (!(Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
+  New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+}
+# Si l'arxiu existeix, canvia config...
+Else {
+  Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+}
+# Altres paràmetres...
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
+
+# Activa accessos directes predeterminats ("mi equipo", brossa...).
+
+$Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
+$Name = @(
+  "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"  # mi equipo
+  "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}"  # xarxa
+  "{59031a47-3f72-44a7-89c5-5595fe6b30ee}"  # carpeta d'usuari
+  "{645FF040-5081-101B-9F08-00AA002F954E}"  # contenidor brossa
+# "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}"  # panell de control
+)
+Write-Host "Creating Desktop Shortcuts..."
+
+foreach ($short in $Name) {
+    $exist = "Get-ItemProperty -Path $Path -Name $Name"
+    if ($exist) {
+        Set-ItemProperty -Path $path -Name $name -Value 0
+    }
+    Else {
+        New-ItemProperty -Path $path -Name $name -Value 0
+    }
+}
+
+
+#############################################################
+
 # Elimina aplicacions mitjançant cmd.exe:
 # AppPackage és molt incomplet, i PowerShell plena d'insectes.
 # CMD /C: vol dir còrrer cmd.exe i quan acabi terminar.
