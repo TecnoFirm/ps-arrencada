ab4a91e705aa273e32e433265a8ff71dc0952934
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-10-10 09:19:17 +0200

    Create guio-pres.ps1

---
+++ guio-pres.ps1
@@ -0,0 +1,16 @@
+
+# Encarregat d'eliminar totes les dades dels portàtils de prèstec. Reinicialització mínima.
+
+
+# Netejar escriptori i documents de la carpeta d'usuari. Mirar documents oberts recentment.
+## [FALTA]
+# Netejar la carpeta de dades que recol·lecta Google Chrome (i què sobre Edge?)
+## [FALTA]
+# Revisar el núvol del compte MS, i que no han canviat el compte MS per l'Office.
+## [FALTA]
+# Revisar Accés Alumnes.
+## [FALTA]
+# LibreOffice... en algunes instàncies es pot emprar...
+## [FALTA]
+# Aula Virtual o altres aplicacions; eliminar dades d'usuàri.
+## [FALTA]
