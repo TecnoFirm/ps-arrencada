8a9b36b22d969c429e6cad913a3f4a15c9f8ee69
adria-boada <94541540+adria-boada@users.noreply.github.com>
2022-11-30 12:00:18 +0100

    Updated ping script
    Added new arguments: amount of time until analyses finishes (in hours)
    and outfile name to write to results.
    
    Does no longer print too much output.
    
    Keeps a track of the analyses counter, printing to screen.

--- guio-ping.py
+++ guio-ping.py
@@ -49,27 +49,31 @@ if __name__ == '__main__':
     ### PARSE ARGUMENTS ###
     import argparse
 
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(
+        description='Analyse the network via `mtr` for a period of time (by default, a day)')
     # file-name: positional arg.
     parser.add_argument('hostnames', type=str,
     help='Path to hostnames file (each row should correspond to a singular hostname)'
                         )
-    # integer argument
-#    parser.add_argument('-a', '--numero_a', type=int, help='Paràmetre "a"')
-    # choices argument
-#    parser.add_argument('-o', '--operacio', 
-#            type=str, choices=['suma', 'resta', 'multiplicacio'],
-#            default='suma', required=False,
-#            help='')
+    # file-name: optional required argument.
+    parser.add_argument('-o', '--outfile', type=str,
+        help='Path and filename where output should be written (will append to the end)')
+    # integer: not required.
+    parser.add_argument('-t', '--time',
+            type=int, default=int(24), required=False,
+            help="Total time in which analyses will be performed, *in hours*")
 
     args = parser.parse_args()
     # call a value: args.operacio or args.filename.
 
-    # Un dia sencer es podria formar amb 72 intervals de 20 minuts...
-    # O amb 72*2 = 144 intervals de 10 minuts.
-    sleep_interval = 10 #in minutes, between repeats (replicates)
-    repeats = 0
-    while repeats < 72:
+    # How many analysis can be performed in "args.hores" hours, accounting for
+    # spaces of 10 minutes between each analysis?
+    # 6 analyses should be performed per hour.
+    requested_analyses = args.time*6 #int, in replicates. Amount of repeats.
+    sleep_interval = 10 #int, in minutes. Time between repeats (replicates)
+    performed_analyses = 0
+
+    while performed_analyses < requested_analyses:
         with open(args.hostnames) as hfile:
             # init the dataframe to store all hostnames' traceroutes:
             df = pd.DataFrame(columns=[
@@ -94,16 +98,20 @@ if __name__ == '__main__':
                 # elimina la primera i última columna (.iloc[:,1:-1])
                 # la primera te la versió del soft. i la última és buida.
                 df = pd.concat([df, pd.read_csv('traceroute.tmp.csv').iloc[:,1:-1]])
-                print(df)
 
         # Tick forward the counter.
-        repeats += 1
+        performed_analyses += 1
+        print(f'-- Performed analyses counter: {performed_analyses} --')
         # Exporta els resultats *cada vegada* que es faci de nou anàlisi?
-        print('-- Saving pd.DataFrame() to `ping-traceroute.csv` --')
-        df.to_csv('ping-traceroute.csv')
+        print(f'-- Saving pd.DataFrame() to `{args.outfile}` --')
+        df.to_csv(args.outfile)
         # Sleep for 'sleep_interval' minutes until next analyses
         time.sleep(sleep_interval*60)
-    # Output dels resultats a un fitxer.
+    # Imprimeix els resultats finals en pantalla.
     print('-- Final dataframe --'); print()
     print(df) ; print()
+    print('-- Analyses have finished; EOF --')
+    # remove tmp file
+    os.system('rm --force traceroute.tmp.csv')
+    print (f'-- temp. file traceroute.tmp.csv has been removed --')
 
